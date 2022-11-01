9f306ccde0410d928100c86853ac71ef2b4f6329
Naveen K <96463561+thenameiswin@users.noreply.github.com>
2022-11-01 12:48:12 +0530

    Create Mad libs game.py

---
+++ Mad libs game.py
@@ -0,0 +1,6 @@
+color = input("Enter a Color: ")
+pluralNoun = input("Enter a Plural Noun: ")
+celebrity = input("Enter the name of a celebrity: ")
+print("Roses are ", color)
+print(pluralNoun, " is blue")
+print("I like ", celebrity)
